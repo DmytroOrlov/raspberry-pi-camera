768c9854fd89bdbffd7f352f5095956f77f50a96
Dmytro Orlov <orlov.dm@gmail.com>
2022-07-02 22:26:35 +0200

    add release

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ import cv2
 
 app = FastAPI()
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2592)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1944)
-
 @app.get("/")
 def read_root():
   return {"Hello": "World"}
 
 @app.get("/image")
 def image():
+  cap = cv2.VideoCapture(0)
+  cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2592)
+  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1944)
   res, frame = cap.read()
   res, im_jpeg = cv2.imencode(".jpeg", frame)
+  cap.release()
   return StreamingResponse(io.BytesIO(im_jpeg.tobytes()), media_type="image/jpeg")
 
 @app.get("/items/{item_id}")
